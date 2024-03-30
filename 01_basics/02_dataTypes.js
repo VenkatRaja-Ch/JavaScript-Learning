4c68b7a0960d076e3ef8b18c435b247f774592b4
Venkat Raja, Ch <chvenkatraja.work@gmail.com>
2024-03-30 21:15:27 +0200

    different data types in js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as of new version.
+
+// alert("This is a alert messsage!") // This won't work if you try to execute under node environment. You need to perform it in browser.
+
+
+let name = "Venkat Raja Ch" //string
+let age = 24 // number
+let isLoggedIn = true // boolean
+let state = null
+
+// Data Type
+// number => 2^53
+// bigint => usually used for larger interger.
+// boolean => true/false
+// null => stand alone value 
+// undefined => the variable is not defined/assigned yet.
+// symbol => Useful to find uniqueness (usually used in reactJS)
+
+
+// object
+console.log(typeof "Venkat Raja Ch"); // typeof => it is used to find the data type of the variable.
+console.log(typeof state); // the output will be object.
+console.log(typeof undefined); // the output will be undefined as it is a value.

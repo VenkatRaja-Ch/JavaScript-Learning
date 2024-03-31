eba82df1e6de2c62f81d788933a38928167d3eb7
Venkat Raja, Ch <chvenkatraja.work@gmail.com>
2024-04-01 02:07:06 +0300

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,51 @@
+// Primitive Data type (call-by value):
+
+// Types:
+const score = 100                       // Number -> integer values
+const scoreValue = 100.3                // Number -> decimal values
+
+const isLoggedIn = false;               // Boolean Type
+const outsideTemp = null;               // Null type
+let userEmail;                          // Undefined
+
+const id = Symbol('12345');             // Symbol type (used for keeping the value unique)
+const anotherId = Symbol('12345');
+console.log(id === anotherId);          // output: false. Even though the values are same but the Symbol type returns unique numbers
+
+const bigNumber = 43983938893498359n    // Number -> BigInt values; "n" is used in the end to let the JavaScript know that it has BigInt values.
+
+
+
+console.log("---------------------------");
+console.log(typeof score);
+console.log(typeof scoreValue);
+console.log(typeof isLoggedIn);
+console.log(typeof userEmail);
+console.log(typeof id);
+console.log(typeof anotherId);
+console.log(typeof bigNumber);
+console.log("---------------------------");
+
+
+// Non-Primitive Data type (reference type):
+
+// Types:
+// Array type:
+const heros = ["shaktiman", "naagraj", "doga"];
+
+// Objects:
+let myObject = {
+    name: "Venkat Raja Ch",
+    age: 24
+}
+
+// Functions:
+const printHelloWorldFunction = function(){
+    console.log("Hello World!");
+}
+
+// Typeof Operator is used to find the data type.
+// Documentation Link: https://262.ecma-international.org/13.0/#sec-typeof-operator
+console.log(typeof heros);
+console.log(typeof myObject);
+console.log(typeof printHelloWorldFunction);
